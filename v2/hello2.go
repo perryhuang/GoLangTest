68073d06752c67f9fd63cc94482030b7abdaf4c0
Perry Huang <Perry.Huang@amd.com>
2020-12-16 16:44:35 +0800

    v2 commit test

---
+++ v2/hello2.go
@@ -0,0 +1,15 @@
+package hello2
+
+import (
+	"rsc.io/quote/v3"
+)
+
+func Hello() string {
+	return quote.HelloV3()
+	//	return "Hello, world."
+	//	return "Hello, world."
+}
+
+func Proverb() string {
+	return quote.Concurrency()
+}
